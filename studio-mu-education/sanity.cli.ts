41f8e189e0ff103dc230c5b5646ee5dd048a21fd
Hitoshidattebayo <upcoming0216@gmail.com>
2026-01-26 14:09:56 +0800

    chore: add deployed studio appId to config

--- studio-mu-education/sanity.cli.ts
+++ studio-mu-education/sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'powqxdmkfx0o0cl1u2h78x70',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
